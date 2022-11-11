d8e807773ac837114f3e8fe154f85a168de7a885
idstev <johaanc002@gmail.com>
2022-11-11 10:29:45 -0500

    first commit

---
+++ javascriptTest/resources/javascript/script.js
@@ -0,0 +1,23 @@
+console.log("BIENVENIDO A LA EVALUACIÓN DE JAVASCRIPT!!!")
+
+function printText(){
+    let printInfo=document.getElementById("boxText")
+    alert(printInfo)
+    console.log(boxText)
+}
+function updateHeader (){
+    cabec.document=getElementById("cabecera")
+    cabec.setAttribute("class","header2")
+}
+function myFunction(){
+    
+
+
+    if (boxText==""){
+        alert("Ups! Nada que mostrar")
+    }else{
+        alert(printInfo)
+    }
+
+
+}
